dbc6d911e834bb608909f3001cbe29621560ccf7
Sarkis <sarkis.tlt@gmail.com>
2017-03-31 13:56:22 -0700

    gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,13 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import sourcemaps from 'gulp-sourcemaps';
+
+gulp.task('build:js', () => {
+  return gulp.src('./src/index.js')
+  .pipe(sourcemaps.init())
+  .pipe(babel({ presets: ['es2015', 'stage-0', 'stage-1'], compact: true }))
+  .pipe(gulp.dest('./lib'));
+});
+
+gulp.task('default', gulp.parallel('build:js'));
+
