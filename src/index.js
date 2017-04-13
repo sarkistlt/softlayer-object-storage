ed27a9a8ef306404c3c2a7de81b7bb2518a37353
Sarkis <sarkis.tlt@gmail.com>
2017-04-13 12:00:46 -0700

    upload directory

--- src/index.js
+++ src/index.js
@@ -38,22 +38,18 @@ class ObjectStorage {
   removeContainer(name) {
     if (this.removeAccess) {
       return new Promise((resolve, reject) => {
-        try {
-          request.get(this.token, (err, res) => {
-            if (err) return reject(err);
-            const containerName = name.split('/')[0] ? name.split('/').reverse()[0] : name;
-            const token = {
-              url: `${JSON.parse(res.body).storage[this.storage]}/${containerName}`,
-              headers: { 'X-Auth-Token': res.headers['x-auth-token'] },
-            };
-            request.delete(token, (error) => {
-              if (error) reject({ error, done: true });
-              resolve({ done: true });
-            });
+        request.get(this.token, (err, res) => {
+          if (err) return reject(err);
+          const containerName = name.split('/')[0] ? name.split('/').reverse()[0] : name;
+          const token = {
+            url: `${JSON.parse(res.body).storage[this.storage]}/${containerName}`,
+            headers: { 'X-Auth-Token': res.headers['x-auth-token'] },
+          };
+          request.delete(token, (error) => {
+            if (error) reject({ error, done: true });
+            resolve({ done: true });
           });
-        } catch (e) {
-          reject(e);
-        }
+        });
       });
     }
     return Promise.resolve('remove not allowed, set { removeAccess: true } to allow');
@@ -61,56 +57,48 @@ class ObjectStorage {
 
   listContainers() {
     return new Promise((resolve, reject) => {
-      try {
-        request.get(this.token, (err, res) => {
-          const token = {
-            url: `${JSON.parse(res.body).storage[this.storage]}`,
-            headers: { 'X-Auth-Token': res.headers['x-auth-token'] },
-          };
-          request.get(token, (error, { body }) => {
-            if (error) return reject(error);
-            const list = [];
-            body.split('\n').forEach((name) => {
-              if (name) {
-                list.push(`${JSON.parse(res.body).storage[this.storage]}/${name}`);
-              }
-            });
-            resolve(list);
+      request.get(this.token, (err, res) => {
+        const token = {
+          url: `${JSON.parse(res.body).storage[this.storage]}`,
+          headers: { 'X-Auth-Token': res.headers['x-auth-token'] },
+        };
+        request.get(token, (error, { body }) => {
+          if (error) return reject(error);
+          const list = [];
+          body.split('\n').forEach((name) => {
+            if (name) {
+              list.push(`${JSON.parse(res.body).storage[this.storage]}/${name}`);
+            }
           });
+          resolve(list);
         });
-      } catch (e) {
-        reject(e);
-      }
+      });
     });
   }
 
   listFiles(path) {
     return new Promise((resolve, reject) => {
-      try {
-        request.get(this.token, (err, res) => {
-          const token = {
-            url: `${JSON.parse(res.body).storage[this.storage]}/${this.container}`,
-            headers: { 'X-Auth-Token': res.headers['x-auth-token'] },
-          };
-          request.get(token, (error, { body }) => {
-            if (error) return reject(error);
-            const list = [];
-            body.split('\n').forEach((name) => {
-              if (name) {
-                const url = `${JSON.parse(res.body).storage[this.storage]}/${this.container}/${name}`;
-                if (path) {
-                  if (~url.indexOf(`${this.container}${path}`)) list.push(url);
-                } else {
-                  list.push(url);
-                }
+      request.get(this.token, (err, res) => {
+        const token = {
+          url: `${JSON.parse(res.body).storage[this.storage]}/${this.container}`,
+          headers: { 'X-Auth-Token': res.headers['x-auth-token'] },
+        };
+        request.get(token, (error, { body }) => {
+          if (error) return reject(error);
+          const list = [];
+          body.split('\n').forEach((name) => {
+            if (name) {
+              const url = `${JSON.parse(res.body).storage[this.storage]}/${this.container}/${name}`;
+              if (path) {
+                if (~url.indexOf(`${this.container}${path}`)) list.push(url);
+              } else {
+                list.push(url);
               }
-            });
-            resolve(list);
+            }
           });
+          resolve(list);
         });
-      } catch (e) {
-        reject(e);
-      }
+      });
     });
   }
 
@@ -118,79 +106,71 @@ class ObjectStorage {
     const isFolder = typeof file === 'string' && fs.lstatSync(file).isDirectory();
 
     return new Promise((resolve, reject) => {
-      try {
-        if (isFolder) {
-          const rootFolder = file;
-          const walker = walk.walk(rootFolder, { followLinks: false });
+      if (isFolder) {
+        const rootFolder = file;
+        const walker = walk.walk(rootFolder, { followLinks: false });
 
-          walker.on('end', () => resolve('done'));
-          walker.on('error', reject);
+        walker.on('end', () => resolve('done'));
+        walker.on('error', reject);
 
-          return walker.on('file', (root, stat, next) => {
-            const filePath = `${root}/${stat.name}`;
-            const fileName = filePath.split('/').reverse()[0];
-            const containerPath = filePath.replace(rootFolder, '').replace(fileName, '');
-            console.log(filePath);
-            this.uploadFile(filePath, false, `${this.container}${containerPath}`, next);
-          });
-        }
-
-        const readStream = typeof file === 'string' ? fs.createReadStream(file) : file;
-        const filename = name || readStream.path.split('/').reverse()[0];
-        request.get(this.token, (err, res) => {
-          if (err) return reject(err);
-          const token = {
-            url: `${JSON.parse(res.body).storage[this.storage]}/${container || this.container}/${filename}`,
-            headers: { 'X-Auth-Token': res.headers['x-auth-token'] },
-          };
-          const writeStream = request.put(token);
-          readStream.pipe(writeStream);
-          readStream.on('error', (error) => {
-            if (cb) return cb();
-            reject(error);
-          });
-          readStream.on('close', () => {
-            if (cb) return cb();
-            resolve(token.url);
-          });
+        return walker.on('file', (root, stat, next) => {
+          const filePath = `${root}/${stat.name}`;
+          const fileName = filePath.split('/').reverse()[0];
+          const containerPath = filePath.replace(rootFolder, '').replace(fileName, '').slice(0, -1);
+          console.log(filePath, `${this.container}${containerPath}`);
+          this.uploadFile(filePath, false, `${this.container}${containerPath}`, next);
         });
-      } catch (e) {
-        reject(e);
       }
+
+      const readStream = typeof file === 'string' ? fs.createReadStream(file) : file;
+      const filename = name || readStream.path.split('/').reverse()[0];
+      request.get(this.token, (err, res) => {
+        if (err) return reject(err);
+        const token = {
+          url: `${JSON.parse(res.body).storage[this.storage]}/${container || this.container}/${filename}`,
+          headers: { 'X-Auth-Token': res.headers['x-auth-token'] },
+        };
+        const writeStream = request.put(token);
+        readStream.pipe(writeStream);
+        readStream.on('error', (error) => {
+          if (cb) return cb();
+          reject(error);
+        });
+        readStream.on('close', () => {
+          if (cb) return cb();
+          resolve(token.url);
+        });
+      });
     });
   }
 
   removeFile(files) {
     if (this.removeAccess) {
       return new Promise((resolve, reject) => {
-        try {
-          request.get(this.token, (err, res) => {
-            if (err) return reject(err);
-            if (Array.isArray(files)) {
-              files.forEach((file, idx) => {
-                const filename = file.split('/')[0] ? file.split('/').splice(file.split('/').indexOf(this.container) + 1).join('/') : file;
-                request.delete({
-                  url: `${JSON.parse(res.body).storage[this.storage]}/${this.container}/${filename}`,
-                  headers: { 'X-Auth-Token': res.headers['x-auth-token'] },
-                }, (error) => {
-                  if (error) reject({ error, done: true });
-                  if (idx === files.length - 1) resolve({ done: true });
-                });
-              });
-            } else {
-              const filename = files.split('/')[0] ? files.split('/').splice(files.split('/').indexOf(this.container) + 1).join('/') : files;
+        request.get(this.token, (err, res) => {
+          if (err) return reject(err);
+          if (Array.isArray(files)) {
+            files.forEach((file, idx) => {
+              const filename = file.split('/')[0] ? file.split('/').splice(file.split('/').indexOf(this.container) + 1).join('/') : file;
               request.delete({
                 url: `${JSON.parse(res.body).storage[this.storage]}/${this.container}/${filename}`,
                 headers: { 'X-Auth-Token': res.headers['x-auth-token'] },
               }, (error) => {
                 if (error) reject({ error, done: true });
-                resolve({ done: true });
+                if (idx === files.length - 1) resolve({ done: true });
               });
-            }
-          });
-        } catch (e) {
-          reject(e);
-        }
+            });
+          } else {
+            const filename = files.split('/')[0] ? files.split('/').splice(files.split('/').indexOf(this.container) + 1).join('/') : files;
+            request.delete({
+              url: `${JSON.parse(res.body).storage[this.storage]}/${this.container}/${filename}`,
+              headers: { 'X-Auth-Token': res.headers['x-auth-token'] },
+            }, (error) => {
+              if (error) reject({ error, done: true });
+              resolve({ done: true });
+            });
+          }
+        });
       });
     }
     return Promise.resolve('remove not allowed, set { removeAccess: true } to allow');
